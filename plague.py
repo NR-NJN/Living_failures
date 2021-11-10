cda9047c6f536d3891cbdfabb62187d754360ab9
Heisenberg <66485604+NR-NJN@users.noreply.github.com>
2021-11-10 14:06:03 +0530

    Add files via upload
    this source code is called plague because it was a plague to write this,

---
+++ plague.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import os
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+student_files = [doc for doc in os.listdir() if doc.endswith('.txt')]
+student_notes =[open(File).read() for File in  student_files]
+
+vectorize = lambda Text: TfidfVectorizer().fit_transform(Text).toarray()
+similarity = lambda doc1, doc2: cosine_similarity([doc1, doc2])
+
+vec = vectorize(student_notes)
+s_vectors = list(zip(student_files, vec))
+plague_res = set()                                               #THIS IS THE TRUE FUCKING PHANTOM PAIN
+                                                        #SO BASICALLY WHAT I HAVE DONE IS
+                                                        #I HAVE USED A METRIC CALLED COSINE SIMILARITY
+                                                        #IF YOU DON'T KNOW WHAT COSINE SIMILARITY MEANS, 
+                                                        #THEN YOU ARE PROBABLY JUST AS DUMB AS I WAS 4 HOURS AGO
+                                                        #BASICALLY IT MEASURES COS OF THE ANGLE BETWEEN 2 VECTORS
+                                                        #PROJECTED IN A MULTIDIMENSIONAL SPACE
+                                                        #THE VECTORS HERE ARE BASICALLY ARRAYS THAT CONTAIN PARAMETERS
+                                                        #(WORD COUNT)(DISTANCE)(PLACEMENTS)
+                                                        #NOW IF THIS CHECKER SOUNDS TOO PRIMITIVE, THEN
+                                                        #WELL IM SORRY, BUT I HAD ONLY 2 DAYS TO COME UP WITH THIS
+                                                        #I HOPE YOU ARE AWARE OF THE FORMULA cos(theta)=a.b/|a|.|b|
+                                                        #THIS IS BASICALLY THE LOGICAL FORMULA THIS PROGRAM USES TO CALCULATE SIMILARITIES
+                                                        #OTHER THAN THIS, I HAVE MODIFIED THIS PROGRAM TO CHECK THIS SPECIFIC FOLDER FOR ANY TEXT FILES
+                                                        #I HAD SAVED 3 TEXT FILES, SO NOW IT WILL RETURN THE COS SIMILARITY 
+                                                        #BETWEEN THOSE 3 FILES. RANGES FROM 0 TO 1, JUST LIKE PROBABILITIES
+                                                        #THE REST OF THE RULES ARE UPTO YOU
+                                                        
+                                                        
+ 
+def check_plague():
+    global s_vectors
+    for student_a, text_vector_a in s_vectors:
+        new_vectors =s_vectors.copy()
+        current_index = new_vectors.index((student_a, text_vector_a))
+        del new_vectors[current_index]
+        for student_b , text_vector_b in new_vectors:
+            sim_score = similarity(text_vector_a, text_vector_b)[0][1]
+            student_pair = sorted((student_a, student_b))
+            score = (student_pair[0], student_pair[1],sim_score)
+            plague_res.add(score)
+    return plague_res
+
+for data in check_plague():
+    print(data) 
