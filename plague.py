96551f70e9c58e2b4ae0c973d7686598711f4d1c
Heisenberg <66485604+NR-NJN@users.noreply.github.com>
2021-11-12 09:46:43 +0530

    Add files via upload

--- plague.py
+++ plague.py
@@ -1,37 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
 
-import sys
-import math
-import re
-from collections import Counter
+This is a temporary script file.
+"""
 
-WORD = re.compile(r"\w+")
+import os
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+os.chdir()
+student_files = [doc for doc in os.listdir() if doc.endswith('.txt')]
+student_notes =[open(File).read() for File in  student_files]
 
+vectorize = lambda Text: TfidfVectorizer().fit_transform(Text).toarray()
+similarity = lambda doc1, doc2: cosine_similarity([doc1, doc2])
 
-def get_cosine(vec1, vec2):
-    intersection = set(vec1.keys()) & set(vec2.keys())
-    numerator = sum([vec1[x] * vec2[x] for x in intersection])
+vec = vectorize(student_notes)
+s_vectors = list(zip(student_files, vec))
+plague_res = set()                                               
+                                                      
+                                                        
+                                                        
+ 
+def check_plague():
+    global s_vectors
+    for student_a, text_vector_a in s_vectors:
+        new_vectors =s_vectors.copy()
+        current_index = new_vectors.index((student_a, text_vector_a))
+        del new_vectors[current_index]
+        for student_b , text_vector_b in new_vectors:
+            sim_score = similarity(text_vector_a, text_vector_b)[0][1]
+            percentage="{:.3%}".format(sim_score)
+            student_pair = sorted((student_a, student_b))
+            score = (student_pair[0], student_pair[1],percentage)
+            plague_res.add(score)
+    return plague_res
 
-    sum1 = sum([vec1[x] ** 2 for x in list(vec1.keys())])
-    sum2 = sum([vec2[x] ** 2 for x in list(vec2.keys())])
-    denominator = math.sqrt(sum1) * math.sqrt(sum2)
-
-    if not denominator:
-        return 0.0
-    else:
-        return float(numerator) / denominator
-
-
-def text_to_vector(text):
-    words = WORD.findall(text)
-    return Counter(words)
-
-
-prog=sys.argv[0]
-arg1=sys.argv[1]
-arg2=sys.argv[2]
-vector1 = text_to_vector(arg1)
-vector2= text_to_vector(arg2)
-cosine = get_cosine(vector1, vector2)
-percentage="{:.3%}".format(cosine)
-
-print("Cosine:", percentage)
+for data in check_plague():
+    print(data) 
